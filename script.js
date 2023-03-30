2bdd387021f219d46b15d7732662ddf2cb33d67d
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-03-30 22:44:27 +0300

    Summary (required)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+import { save, load } from "./localstorage.js";
+window.addEventListener("DOMContentLoaded", fillTaskList);
+
+const STORAGE_KEY = "tasks";
+let currentId = 0;
+
 // Create a "close" button and append it to each list item
 const myNodelist = document.getElementsByTagName("LI");
 for (let i = 0; i < myNodelist.length; i++) {
@@ -8,22 +14,27 @@ for (let i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to remove the current list item
-const close = document.getElementsByClassName("close");
-for (let i = 0; i < close.length; i++) {
-  close[i].onclick = function () {
-    const div = this.parentElement;
-    div.remove();
-  };
-}
-
 // Add a "checked" symbol when clicking on a list item
 const list = document.getElementById("myUL");
 list.addEventListener(
   "click",
   function (ev) {
+    const currentState = load(STORAGE_KEY);
     if (ev.target.tagName === "LI") {
       ev.target.classList.toggle("checked");
+      const taskIndex = currentState.findIndex(
+        (task) => Number(task.id) === Number(ev.target.dataset.id)
+      );
+      currentState[taskIndex].isDone = !currentState[taskIndex].isDone;
+      save(STORAGE_KEY, currentState);
+    } else if (ev.target.tagName === "SPAN") {
+      const li = ev.target.parentNode;
+      li.remove();
+      const taskIndex = currentState.findIndex(
+        (task) => Number(task.id) === Number(li.dataset.id)
+      );
+      currentState.splice(taskIndex, 1);
+      save(STORAGE_KEY, currentState); // оновити масив в локальному сховищі
     }
   },
   false
@@ -31,15 +42,60 @@ list.addEventListener(
 
 // Create a new list item when clicking on the "Add" button
 const addBtn = document.querySelector(".addBtn");
-addBtn.addEventListener("click", function () {
-  const inputValue = document.getElementById("myInput").value;
-  if (!inputValue.trim()) {
-    alert("You must write something!");
-    return;
+addBtn.addEventListener(
+  "click",
+  function (text, isDone = false, id = currentId) {
+    const inputValue = document.getElementById("myInput").value;
+    if (!inputValue.trim()) {
+      alert("You must write something!");
+      return;
+    }
+    addTaskToStorage(inputValue);
+    const li = document.createElement("li");
+    const t = document.createTextNode(inputValue);
+    li.className = "todo-list";
+    li.dataset.id = id;
+    if (isDone) li.classList.add("checked");
+    li.appendChild(t);
+    document.getElementById("myUL").appendChild(li);
+    document.getElementById("myInput").value = "";
+
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
   }
+);
+
+// localStorage
+
+function createTaskObj(text, isDone) {
+  return {
+    text,
+    isDone,
+    id: currentId,
+  };
+}
+
+function addTaskToStorage(text, isDone = false) {
+  const currentState = load(STORAGE_KEY);
+  if (currentState === undefined) {
+    const arr = [createTaskObj(text, isDone)];
+    save(STORAGE_KEY, arr);
+  } else {
+    currentState.push(createTaskObj(text, isDone));
+    save(STORAGE_KEY, currentState);
+  }
+  currentId += 1;
+}
+
+function createLi(text, isDone, id) {
   const li = document.createElement("li");
-  const t = document.createTextNode(inputValue);
+  const t = document.createTextNode(text);
   li.className = "todo-list";
+  li.dataset.id = id;
+  if (isDone) li.classList.add("checked");
   li.appendChild(t);
   document.getElementById("myUL").appendChild(li);
   document.getElementById("myInput").value = "";
@@ -49,11 +105,15 @@ addBtn.addEventListener("click", function () {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
+}
 
-  for (let i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-      const div = this.parentElement;
-      div.remove();
-    };
+// Fill task list with data from local storage
+function fillTaskList() {
+  const currentState = load(STORAGE_KEY);
+  if (currentState !== undefined) {
+    currentState.forEach(({ text, isDone, id }) => {
+      createLi(text, isDone, id);
+      currentId = id + 1;
+    });
   }
-});
+}
